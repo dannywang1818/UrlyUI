1b591f570ed9141910e328360f3a6a07ddb1884d
TIANHAO WANG <tianhao@TIANHAOs-MacBook-Pro.local>
2021-12-22 20:26:04 -0800

    Added Local Storage & Deleted Button & Jest Testing

--- urly-front-end/src/components/frontpage/UrlForm.jsx
+++ urly-front-end/src/components/frontpage/UrlForm.jsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from "react";
-import getShortUrl from "../data/ShortUrls";
+import getShortUrl from "../../data/ShortUrls";
 import UrlList from "./UrlList";
 import { CopyToClipboard } from "react-copy-to-clipboard";
-import UrlValidator from "../utils/UrlValidator";
+import UrlValidator from "../../utils/UrlValidator";
 
 export default function UrlForm() {
   const MAX_URLS_NUMBER = 3;
   const MAX_URL_LENGTH = 40;
 
-  const [urls, setUrls] = useState([]);
-  const [Id, setId] = useState(1);
+  const [urls, setUrls] = useState(() => {
+    const savedUrls = localStorage.getItem("url");
+    const initialUrls = JSON.parse(savedUrls);
+    return initialUrls || [];
+  });
+  const [Id, setId] = useState(() => {
+    const savedUrls = localStorage.getItem("url");
+    const initialUrls = JSON.parse(savedUrls);
+    if (initialUrls != null && initialUrls.length > 0) {
+      return initialUrls[0].Id + 1;
+    }
+    return 1;
+  });
   const [buttonText, setButtonText] = useState("Shorten");
   const [buttonColor, setButtonColor] = useState(
     "bg-indigo-600 hover:bg-indigo-700 "
@@ -19,7 +30,7 @@ export default function UrlForm() {
   const [showWarning, setShowWarning] = useState("");
 
   const updateButtonColor = (curColor) => {
-    if (curColor == "bg-indigo-600 hover:bg-indigo-700 ") {
+    if (curColor === "bg-indigo-600 hover:bg-indigo-700 ") {
       return "bg-green-600 hover:bg-green-600 ";
     } else {
       return "bg-indigo-600 hover:bg-indigo-700 ";
@@ -27,9 +38,9 @@ export default function UrlForm() {
   };
 
   const updateButtonText = (curText) => {
-    if (curText == "Shorten") {
+    if (curText === "Shorten") {
       return "Copy";
-    } else if (curText == "Copy") {
+    } else if (curText === "Copy") {
       return "Copied!";
     } else {
       return "Copy";
@@ -37,7 +48,7 @@ export default function UrlForm() {
   };
 
   const handleClick = (e) => {
-    if (buttonText == "Shorten") {
+    if (buttonText === "Shorten") {
       let longUrl = inputValue;
       let response = UrlValidator(longUrl);
       if (!response.state) {
@@ -56,29 +67,45 @@ export default function UrlForm() {
           short: getShortUrl(Id),
         },
       ];
-      if (urls != undefined && urls.length >= MAX_URLS_NUMBER) {
+      if (urls !== undefined && urls.length >= MAX_URLS_NUMBER) {
         urls.pop();
       }
       updatedUrls.push(...urls);
       setUrls(updatedUrls);
       setId((prevId) => prevId + 1);
+      localStorage.setItem("url", JSON.stringify(updatedUrls));
     }
 
-    if (buttonText == "Copy") {
+    if (buttonText === "Copied!") {
+      return;
+    }
+
+    if (buttonText === "Copy") {
       setButtonColor(updateButtonColor(buttonColor));
     }
     setButtonText(updateButtonText(buttonText));
   };
 
+  const handleDelete = (Id) => {
+    let updatedUrls = [];
+    for (let i = 0; i < urls.length; i++) {
+      if (urls[i].Id !== Id) {
+        updatedUrls.push(urls[i]);
+      }
+    }
+    setUrls([...updatedUrls]);
+    localStorage.setItem("url", JSON.stringify(updatedUrls));
+  };
+
   const handleUrlChange = (e) => {
     setInputValue(e.target.value);
-    if (buttonText == "Copy") {
+    if (buttonText === "Copy") {
       setButtonText("Shorten");
     }
   };
 
   useEffect(() => {
-    if (buttonText == "Copied!") {
+    if (buttonText === "Copied!") {
       setTimeout(() => {
         setButtonText(updateButtonText(buttonText));
         setButtonColor(updateButtonColor(buttonColor));
@@ -98,8 +125,8 @@ export default function UrlForm() {
   return (
     <div className="sm:flex sm:flex-col sm:rounded-lg px-4 py-5 sm:p-6 w-2/3">
       <div className="sm:rounded-lg py-4 sm:flex sm:items-center sm:justify-between w-full">
-        <div className="w-4/5">
-          <label for="url" className="sr-only">
+        <div className="w-10/12">
+          <label htmlFor="url" className="sr-only">
             Url
           </label>
           <input
@@ -112,14 +139,14 @@ export default function UrlForm() {
             placeholder="Shorten your link"
           />
         </div>
-        <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
+        <div className="w-2/12 mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
           <CopyToClipboard text={urls.length > 0 ? urls[0].short : ""}>
             <button
               type="button"
               onClick={handleClick}
               className={
                 buttonColor +
-                "inline-flex items-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-lg"
+                "inline-flex justify-center items-center mr-4 px-4 py-2 w-full border border-transparent shadow-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-lg"
               }
             >
               {buttonText}
@@ -132,7 +159,9 @@ export default function UrlForm() {
           <span className="align-middle text-lg">{showWarning}</span>
         </div>
       )}
-      {urls != undefined && urls.length > 0 && <UrlList urls={urls} />}
+      {urls !== undefined && urls.length > 0 && (
+        <UrlList urls={urls} handleDelete={handleDelete} />
+      )}
     </div>
   );
 }
